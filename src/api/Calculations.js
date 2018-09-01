304dd2e2314e51f34710367b091756337664e3b9
Oz Ramos <labofoz@gmail.com>
2018-08-31 18:37:13 -0700

    #13 Finished the mappedEyeCentroid plugin

--- src/api/Calculations.js
+++ src/api/Calculations.js
@@ -6,7 +6,22 @@
 const forEach = require('lodash/forEach')
 
 module.exports = function (Handsfree) {
+  /**
+   * Normalizes a val between min and max range
+   * @param  {Number} val The value to normalize
+   * @param  {Number} max The minimum range
+   * @param  {Number} min The maximum range
+   * @return {Number}     The normalized value
+   */
   const normalize = (val, max, min) => (val - min) / (max - min)
+
+  /**
+   * Finds the center {x, y} between two {x, y}'s
+   * @param  {Point} a The first point
+   * @param  {Point} b The second point
+   * @return {Point}   A point representing the center of the two
+   */
+  const midpoint = (a, b) => {return {x: (a.x + b.x) / 2, y: (a.y + b.y) / 2}}
 
   /**
    * Run calculations
@@ -15,18 +30,24 @@ module.exports = function (Handsfree) {
     this.poses && this.poses.forEach((pose, index) => {
       const partNames = ['nose', 'eyeR', 'eyeL']
 
-      // Core calculations
+      // Adds calcuations to pose.part by partName
       pose.part = {}
       partNames.forEach((partName, i) => {
+        // Add Properties
         const position = pose.keypoints[i].position
         pose.part[partName] = {
           position,
+          score: pose.keypoints[i].score,
+          // Canvas Normals: each pixel the canvas is between 0 and 1
           normal: {
-            x: normalize(position.x, 0, window.outerWidth),
-            y: normalize(position.x, 0, window.outerHeight)
+            x: normalize(position.x, 0, this.canvas.width),
+            y: normalize(this.canvas.height - position.y, 0, this.canvas.height)
           }
         }
       })
+
+      // Calculate eye midpoint
+      pose.part.eyeMidpoint = midpoint(pose.part.eyeL.normal, pose.part.eyeR.normal),
 
       // @FIXME let's only calculate the one's we need, like only the eyes and nose
       this.poses[index] = pose
--- src/plugins/mappedEyeCentroid.js
+++ src/plugins/mappedEyeCentroid.js
@@ -5,14 +5,19 @@ const $pointer = document.createElement('div')
 $pointer.classList.add('handsfree-plugin-mappedEyeCentroid-pointer')
 $pointer.style.display = 'none'
 $pointer.style.position = 'fixed'
-$pointer.style.width = '20px'
-$pointer.style.height = '20px'
-$pointer.style.borderRadius = '20px'
-$pointer.style.background = 'rgba(0, 0, 0, 0.25)'
+$pointer.style.width = '30px'
+$pointer.style.height = '30px'
+$pointer.style.borderRadius = '30px'
+$pointer.style.background = 'rgba(0,0,0,0)'
+$pointer.style.border = '3px dashed rgba(0,0,0,0.35)'
 document.body.appendChild($pointer)
 
 module.exports = {
   name: 'mappedEyeCentroid',
-  onStart (pose) {$pointer.style.display = 'block'},
-  onStop (pose) {$pointer.style.display = 'none'}
+  onStart () {$pointer.style.display = 'block'},
+  onStop () {$pointer.style.display = 'none'},
+  callback (pose) {
+    $pointer.style.left = `${pose.part.eyeMidpoint.x * window.outerWidth}px`
+    $pointer.style.top = `${pose.part.eyeMidpoint.y * window.outerHeight}px`
+  }
 }
